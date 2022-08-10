8392cb6275b96aee68c3d21c94eaa23704821dce
Souvik Sen <82510209+Yourstruggle11@users.noreply.github.com>
2022-08-10 21:00:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.get("/get-single-payslip/:id",async(req,res)=>{
 
 
 
-const CONNECTION_URL = 'mongodb+srv://quordnet:quordnet-1234@cluster0.mhmaf.mongodb.net/MatriculaMockTest?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://quordnet:quordnet-1234@cluster0.mhmaf.mongodb.net/HRMTool?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
